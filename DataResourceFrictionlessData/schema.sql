c1dc73306f881a3704fc294fa60d97f6ff772e88
Alberto Abella <alberto.abella@fiware.org>
2023-10-26 15:06:10 +0200

    test of SQL export

--- DataResourceFrictionlessData/schema.sql
+++ DataResourceFrictionlessData/schema.sql
@@ -1,3 +1,3 @@
 /* (Beta) Export of data model DataResourceFrictionlessData of the subject dataModel.FrictionlessData for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE DataResourceFrictionlessData_type AS ENUM ('DataResourceFrictionlessData');
-CREATE TABLE DataResourceFrictionlessData (address JSON, areaServed TEXT, bytes NUMERIC, data JSON, description TEXT, encoding TEXT, format TEXT, hash TEXT, homepage TEXT, licenses JSON, mediatype TEXT, name TEXT, path JSON, profile TEXT, schema JSON, sources JSON, title TEXT, type DataResourceFrictionlessData_type);
+CREATE TABLE DataResourceFrictionlessData (address JSON, areaServed TEXT, bytes NUMERIC, data JSON, description TEXT, encoding TEXT, format TEXT, hash TEXT, homepage TEXT, id TEXT PRIMARY KEY, licenses JSON, location JSON, mediatype TEXT, name TEXT, path JSON, profile TEXT, schema JSON, sources JSON, title TEXT, type DataResourceFrictionlessData_type);
