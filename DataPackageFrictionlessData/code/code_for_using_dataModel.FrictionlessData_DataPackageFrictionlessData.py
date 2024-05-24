90f9ca87b808ea672a812f1c62ee3039d378b0ab
Alberto Abella <alberto.abella@fiware.org>
2024-05-24 17:31:15 +0200

    created example of code

--- DataPackageFrictionlessData/code/code_for_using_dataModel.FrictionlessData_DataPackageFrictionlessData.py
+++ DataPackageFrictionlessData/code/code_for_using_dataModel.FrictionlessData_DataPackageFrictionlessData.py
@@ -24,31 +24,36 @@
 #         curl -X GET http://localhost:1026/ngsi-ld/v1/entities?local=true&limit=1000
 #         
 #         # now the python code you can use to insert some value in the context broker according to the data model
+#         # Version Warning! 
+#         # This code is designed to work with the version 0.8 of pysmartdatamodels or later
+#         # to work with earlier version you need to replace the import instruction for
+#         # from pysmartdatamodels import pysmartdatamodels as sdm
 #         
-from pysmartdatamodels import pysmartdatamodels as sdm
+#         
+import pysmartdatamodels as sdm
 import subprocess
 serverUrl = "http://localhost:1026" # supposed that your broker is installed in localhost. Edit to match your configuration
 dataModel = "DataPackageFrictionlessData"
 subject = "dataModel.FrictionlessData"
-keywords = {'type': 'Property', 'value': ['CPI', 'World', 'Consumer Price Index', 'Annual Data', 'The World Bank']}
+keywords = ['CPI', 'World', 'Consumer Price Index', 'Annual Data', 'The World Bank']
 attribute = "keywords"
 value = keywords
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-licenses = {'type': 'Property', 'value': [{'name': 'CC-BY-4.0', 'title': 'Creative Commons Attribution 4.0', 'path': 'https://creativecommons.org/licenses/by/4.0/'}]}
+licenses = [{'name': 'CC-BY-4.0', 'title': 'Creative Commons Attribution 4.0', 'path': 'https://creativecommons.org/licenses/by/4.0/'}]
 attribute = "licenses"
 value = licenses
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-profile = "{'type': 'Property', 'value': 'tabular-data-package'}"
+profile = "tabular-data-package"
 attribute = "profile"
 value = profile
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-resources = {'type': 'Property', 'value': [{'path': 'data/cpi.csv', 'name': 'cpi', 'profile': 'tabular-data-resource', 'schema': {'fields': [{'name': 'Country Name', 'type': 'string'}, {'name': 'Country Code', 'type': 'string'}, {'name': 'Year', 'type': 'year'}, {'name': 'CPI', 'description': 'CPI (where 2005=100)', 'type': 'number'}]}}]}
+resources = [{'path': 'data/cpi.csv', 'name': 'cpi', 'profile': 'tabular-data-resource', 'schema': {'fields': [{'name': 'Country Name', 'type': 'string'}, {'name': 'Country Code', 'type': 'string'}, {'name': 'Year', 'type': 'year'}, {'name': 'CPI', 'description': 'CPI (where 2005=100)', 'type': 'number'}]}}]
 attribute = "resources"
 value = resources
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
