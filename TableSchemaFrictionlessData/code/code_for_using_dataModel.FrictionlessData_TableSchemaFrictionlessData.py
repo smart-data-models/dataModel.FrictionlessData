7a4e6c1ae9cb555e8ca195474e1c32339f09e847
Alberto Abella <alberto.abella@fiware.org>
2024-05-24 17:31:22 +0200

    created example of code

--- TableSchemaFrictionlessData/code/code_for_using_dataModel.FrictionlessData_TableSchemaFrictionlessData.py
+++ TableSchemaFrictionlessData/code/code_for_using_dataModel.FrictionlessData_TableSchemaFrictionlessData.py
@@ -24,19 +24,24 @@
 #         curl -X GET http://localhost:1026/ngsi-ld/v1/entities?local=true&limit=1000
 #         
 #         # now the python code you can use to insert some value in the context broker according to the data model
+#         # Version Warning! 
+#         # This code is designed to work with the version 0.8 of pysmartdatamodels or later
+#         # to work with earlier version you need to replace the import instruction for
+#         # from pysmartdatamodels import pysmartdatamodels as sdm
 #         
-from pysmartdatamodels import pysmartdatamodels as sdm
+#         
+import pysmartdatamodels as sdm
 import subprocess
 serverUrl = "http://localhost:1026" # supposed that your broker is installed in localhost. Edit to match your configuration
 dataModel = "TableSchemaFrictionlessData"
 subject = "dataModel.FrictionlessData"
-fields = {'type': 'Property', 'value': [{'name': 'first_name', 'type': 'string', 'constraints': {'required': True}}, {'name': 'age', 'type': 'integer'}]}
+fields = [{'name': 'first_name', 'type': 'string', 'constraints': {'required': True}}, {'name': 'age', 'type': 'integer'}]
 attribute = "fields"
 value = fields
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-primaryKey = {'type': 'Property', 'value': ['name']}
+primaryKey = ['name']
 attribute = "primaryKey"
 value = primaryKey
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
